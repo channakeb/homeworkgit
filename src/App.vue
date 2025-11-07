4aa3381a2beb18c3ff6d247073222139129f75b3
channa <channakeb77@gmail.com>
2025-11-07 19:49:49 +0700

    new

--- src/App.vue
+++ src/App.vue
@@ -593,8 +593,8 @@ import HelloWorld from './components/HelloWorld.vue'
          </p>
       </div>
    </div>
-   <script async defer src="https://buttons.github.io/buttons.js"></script>
-   <script src="https://demo.themesberg.com/windster/app.bundle.js"></script>
+   <!-- <script async defer src="https://buttons.github.io/buttons.js"></script>
+   <script src="https://demo.themesberg.com/windster/app.bundle.js"></script> -->
 </div>
 </template>
 
